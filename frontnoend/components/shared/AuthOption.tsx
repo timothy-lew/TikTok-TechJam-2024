f0cbd854e33d8e2d31aa7fd2c6b2cf4a393bdf39
Lim Jia Earn <119128612+LimJiaEarn@users.noreply.github.com>
2024-07-01 00:29:33 +0800

    Fixed login/signup routes bug

--- frontnoend/components/shared/AuthOption.tsx
+++ frontnoend/components/shared/AuthOption.tsx
@@ -1,9 +1,9 @@
 import Link from "next/link"
 
-const AuthOption = ({ title, iconSVG }: { title: string, iconSVG: JSX.Element }) => {
+const AuthOption = ({ title, iconSVG, hrefLink }: { title: string, iconSVG: JSX.Element, hrefLink: string, }) => {
     return (
       <Link
-		href="/login/form"
+		    href={hrefLink}
       	className="flex justify-center items-center w-[300px] px-3 py-2 border border-slate-500 rounded-sm bg-gray-50 hover:bg-gray-100 cursor-pointer"
         >
         
