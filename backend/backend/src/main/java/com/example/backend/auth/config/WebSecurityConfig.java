67d9035337deb7329e034373c5a2dd87cf513f60
Alexlaaa <leealessandro.sg@gmail.com>
2024-07-05 00:40:30 +0800

    feat: Add frontend deployment endpoint

--- backend/backend/src/main/java/com/example/backend/auth/config/WebSecurityConfig.java
+++ backend/backend/src/main/java/com/example/backend/auth/config/WebSecurityConfig.java
@@ -48,7 +48,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://frontend:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://frontend:3000", "https://tiktok-techjam-pearl.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
         configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
