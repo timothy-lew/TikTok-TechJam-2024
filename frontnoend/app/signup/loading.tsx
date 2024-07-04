09e74616cd03ce1e2b51611fd62df6b1cbdf0905
LimJiaEarn <119128612+LimJiaEarn@users.noreply.github.com>
2024-07-04 19:26:41 +0800

    Added loading states

---
+++ frontnoend/app/signup/loading.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import TikTokLoader from "@/components/shared/TiktokLoader"
+
+const SignUpLoadingPage = () => {
+  return (
+    <div className="min-h-screen w-full flex_center">
+      <TikTokLoader />
+    </div>
+  )
+}
+
+export default SignUpLoadingPage
---
+++ frontnoend/app/wallet/loading.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import TikTokLoader from "@/components/shared/TiktokLoader"
+
+const WalletLoadingPage = () => {
+  return (
+    <div className="min-h-screen w-full flex_center">
+      <TikTokLoader />
+    </div>
+  )
+}
+
+export default WalletLoadingPage
